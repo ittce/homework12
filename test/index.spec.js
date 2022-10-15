4763a3afef17b37bb0301195d7f58ccb2a6b76bc
ccpro <20753403@qq.com>
2022-10-15 23:04:31 +0800

    fix: update test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,5 @@
-
-const Router = require('../router');
-
-
-describe('history校验', () => {
-  it('should pass', () => {
+describe('初始化路由', () => {
+  it('default route', () => {
     //路由表
     var routes = [{
       id: ''
@@ -17,7 +13,72 @@ describe('history校验', () => {
 
     //路由器
     var router = new Router(routes, 'next');
-    expect(router.currentRoute).toBe('next')
+    expect(router.currentRoute).toMatchObject({ id: 'next', title: '下一页' })
+  });
+
+  it('route push', () => {
+    //路由表
+    var routes = [{
+      id: ''
+    }, {
+      id: 'index',
+      title: '首页'
+    }, {
+      id: 'next',
+      title: '下一页'
+    }]
+
+    //路由器
+    var router = new Router(routes, 'index');
+    const res = router.push('next', () => { return 'push next'; });
+    expect(res).toBe('push next');
+    expect(router.currentRoute).toMatchObject({
+      id: 'next',
+      title: '下一页'
+    });
+  });
+
+  it('route back', () => {
+    //路由表
+    var routes = [{
+      id: ''
+    }, {
+      id: 'index',
+      title: '首页'
+    }, {
+      id: 'next',
+      title: '下一页'
+    }]
+
+    //路由器
+    var router = new Router(routes, 'index');
+    router.push('next');
+    router.back();
+    expect(router.currentRoute).toMatchObject({
+      id: 'index',
+      title: '首页'
+    });
+  });
+
+  it('route replace', () => {
+    //路由表
+    var routes = [{
+      id: ''
+    }, {
+      id: 'index',
+      title: '首页'
+    }, {
+      id: 'next',
+      title: '下一页'
+    }]
+
+    //路由器
+    var router = new Router(routes, 'index');
+    router.push('next');
+    router.replace('index');
+    expect(router.currentRoute).toMatchObject({
+      id: 'index',
+      title: '首页'
+    });
   });
 });
-
